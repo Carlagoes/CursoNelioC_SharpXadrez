232a4495fd8f09d2b419beb64b339031c8468d17
Carla Goes <carllagoess@gmail.com>
2021-08-05 10:24:20 -0300

    Comecando a implementar tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
+
 
 namespace CursoNelio17Xadrez
 {
@@ -7,10 +8,7 @@ namespace CursoNelio17Xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
 
 
---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QteMovimento { get; protected set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.Posicao = posicao;
+            this.Cor = cor;
+            this.Tab = tab;
+            this.QteMovimento = 0;
+        }
+    }
+}
